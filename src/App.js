503781d33234dd9d88b856799a61b50485e15f13
Stacy Lafume <stacylafume123@gmail.com>
2023-07-15 00:30:27 -0400

    finished foundation

--- src/App.js
+++ src/App.js
@@ -10,28 +10,58 @@ import Services from './pages/Services';
 import PastEvents from './pages/PastEvents';
 import Events from './pages/Events';
 import EventGallery from './pages/EventGallery';
+import { createTheme, CssBaseline, ThemeProvider, } from '@mui/material';
+import { Button } from '@mui/material';
 
 
 
 const App = () => {
+
+  const theme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#161616',
+        contrastText: 'rgba(255,255,255,0.87)',
+      },
+      secondary: {
+        main: '#ff6d00',
+      },
+      warning: {
+        main: '#ffd54f',
+      },
+      info: {
+        main: '#ff9e80',
+      },
+      error: {
+        main: '#f44336',
+      },
+      success: {
+        main: '#ff6e40',
+      },
+    },
+  })
+
   return (
-  <>
-  <head>
-  <meta name="viewport" content="initial-scale=1, width=device-width" />
-  </head>
-  <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/aboutus" element={<AboutUs />}/>
-    <Route path="/artistofthemonth" element={<ArtistOfTheMonth />}/>
-    <Route path="/ourmission" element={<OurMission/>}/>
-    <Route path="/contactus" element={<ContactUs />}/>
-    <Route path="/services" element={<Services/>}/>
-    <Route path="/pastevents" element={<PastEvents />}/>
-    <Route path="/pastexhibitions" element={<PastExhb />}/>
-    <Route path="/events" element={<Events />}/>
-    <Route path="/eventgallery" element={<EventGallery />}/>
-  </Routes>
-  </>)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {/* <head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </head> */}
+      <Button variant="contained" >Hello worlds</Button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/artistofthemonth" element={<ArtistOfTheMonth />} />
+        <Route path="/ourmission" element={<OurMission />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/pastevents" element={<PastEvents />} />
+        <Route path="/pastexhibitions" element={<PastExhb />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/eventgallery" element={<EventGallery />} />
+      </Routes>
+    </ThemeProvider>)
 }
 
 export default App
