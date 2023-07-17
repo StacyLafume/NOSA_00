09c9dcb882447773ab54b37776c530cb7b3a8cae
Stacy Lafume <stacylafume123@gmail.com>
2023-07-17 19:04:22 -0400

    completed the grids

--- src/App.js
+++ src/App.js
@@ -15,11 +15,62 @@ import { Button } from '@mui/material';
 import MasonryGrid from './components/MasonryGrid';
 import StandardGrid from './components/StandardGrid'
 import Nav from './components/Nav'
+import FilterGrid from './components/FilterGrid';
+
 
 
 
 const App = () => {
-
+  const itemData = [
+    {
+      img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+      title: "Breakfast",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+      title: "Burger",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+      title: "Camera",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+      title: "Coffee",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+      title: "Hats",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+      title: "Honey",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+      title: "Basketball",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+      title: "Fern",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+      title: "Mushrooms",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+      title: "Tomato basil",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+      title: "Sea star",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+      title: "Bike",
+    },
+  ];
   const theme = createTheme({
     palette: {
       mode: 'light',
@@ -48,13 +99,6 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <MasonryGrid/>
-      <StandardGrid/>
-      <Nav/>
-      {/* <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head> */}
-      <Button variant="contained" >Hello worlds</Button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
