1c4b5c13ec7611c5b832873f48d010cc64db19a1
Travora SC <102001997+TravoraSC@users.noreply.github.com>
2023-10-16 01:00:03 -0400

    Calendar/events_TS (#23)
    * Worked on calendar. Using Full Calendar now. Working towards using image background for days with single event.
    
    * Worked out merge conflicts.
    
    * Commented out code causing carousel-ref error.
    
    * Commented out code causing handleClick error.

--- public/main.js
+++ public/main.js
@@ -48,57 +48,57 @@ const formatDate = (date) => {
 };
 
 const processArtistOfTheMonthMarkdownFile = (filePath) => {
-    try {
-        const markdownContent = fs.readFileSync(filePath, 'utf-8');
-        const data = grayMatter(markdownContent).data;
-    
-        const dateString = data.date ? data.date.toString() : new Date().toISOString();
-        const parsedDate = formatDate(dateString);
-        const exhibitionDate = `${parsedDate.monthName} ${parsedDate.day}, ${parsedDate.year}`;
-        const datestring = `${parsedDate.year}-${parsedDate.month}-${parsedDate.day}T${parsedDate.time}:00`;
-        const date = new Date(datestring);
-        const timestamp = date.getTime() / 1000;
+  try {
+    const markdownContent = fs.readFileSync(filePath, 'utf-8');
+    const data = grayMatter(markdownContent).data;
 
-        const artist = {
-          id: timestamp,
-          artist_name: data.artist_name ? data.artist_name : 'No Artist Name given',
-          exhibition_name: data.exhibition_name ? data.exhibition_name : 'No Exhibition Name given',
-          exhibition_date: data.exhibition_date ? data.exhibition_date : 'No Exhibition Date given',
-          artist_statement: data.artist_statement || 'No artist statement given', // Use default value if data.artist_statement is missing or falsy
-          exhibition_statement: data.exhibition_statement || 'No exhibition statement given', // Use default value if data.exhibition_statement is missing or falsy
-          headshot: data.headshot || 'No Headshot given', // Use default value if data.headshot is missing or falsy
-          exhibition_poster: data.exhibition_poster || 'No main Exhibition Poster given', // Use default value if data.main_artwork is missing or falsy
-          exhibition_pieces: data.exhibition_pieces || 'No main Exhibition Pieces given', // Use default value if data.main_artwork is missing or falsy
-        };
-    
-        return artist;
-      } catch (error) {
-        console.error('Error reading Markdown file:', error);
-        return null;
-      }
+    const dateString = data.date ? data.date.toString() : new Date().toISOString();
+    const parsedDate = formatDate(dateString);
+    const exhibitionDate = `${parsedDate.monthName} ${parsedDate.day}, ${parsedDate.year}`;
+    const datestring = `${parsedDate.year}-${parsedDate.month}-${parsedDate.day}T${parsedDate.time}:00`;
+    const date = new Date(datestring);
+    const timestamp = date.getTime() / 1000;
+
+    const artist = {
+      id: timestamp,
+      artist_name: data.artist_name ? data.artist_name : 'No Artist Name given',
+      exhibition_name: data.exhibition_name ? data.exhibition_name : 'No Exhibition Name given',
+      exhibition_date: data.exhibition_date ? data.exhibition_date : 'No Exhibition Date given',
+      artist_statement: data.artist_statement || 'No artist statement given', // Use default value if data.artist_statement is missing or falsy
+      exhibition_statement: data.exhibition_statement || 'No exhibition statement given', // Use default value if data.exhibition_statement is missing or falsy
+      headshot: data.headshot || 'No Headshot given', // Use default value if data.headshot is missing or falsy
+      exhibition_poster: data.exhibition_poster || 'No main Exhibition Poster given', // Use default value if data.main_artwork is missing or falsy
+      exhibition_pieces: data.exhibition_pieces || 'No main Exhibition Pieces given', // Use default value if data.main_artwork is missing or falsy
+    };
+
+    return artist;
+  } catch (error) {
+    console.error('Error reading Markdown file:', error);
+    return null;
+  }
 };
 
 const processEventMarkdownFile = (filePath) => {
-    try {
-        const markdownContent = fs.readFileSync(filePath, 'utf-8');
-        const data = grayMatter(markdownContent).data;
-  
-        const event = {
-            event_name: data.event_name,
-            event_poster: data.event_poster,
-            event_link: data.event_link,
-            event_start_date: data.event_start_date,
-            event_end_date: data.event_end_date,
-            event_pictures: data.event_pictures,
-            event_color: data.event_color,
-            event_description: data.event_description
-        };
-    
-        return event;
-      } catch (error) {
-        console.error('Error reading Markdown file:', error);
-        return null;
-      }
+  try {
+    const markdownContent = fs.readFileSync(filePath, 'utf-8');
+    const data = grayMatter(markdownContent).data;
+
+    const event = {
+      event_name: data.event_name,
+      event_poster: data.event_poster,
+      event_link: data.event_link,
+      event_start_date: data.event_start_date,
+      event_end_date: data.event_end_date,
+      event_pictures: data.event_pictures,
+      event_color: data.event_color,
+      event_description: data.event_description
+    };
+
+    return event;
+  } catch (error) {
+    console.error('Error reading Markdown file:', error);
+    return null;
+  }
 };
 
 const processBannerMarkdownFile = (filePath) => {
@@ -195,11 +195,11 @@ const getSortedArtistList = () => {
     const artistData = processArtistOfTheMonthMarkdownFile(filePath);
 
     if (artistData) {
-        if (artistData.title === 'No title given') {
-            console.error("No title given") 
-          }else{
-              artistList.push(artistData);
-          }
+      if (artistData.title === 'No title given') {
+        console.error("No title given")
+      } else {
+        artistList.push(artistData);
+      }
     }
   });
 
@@ -211,30 +211,30 @@ const getSortedArtistList = () => {
 };
 
 const getSortedEventsList = () => {
-    const dirPath = path.join(__dirname, '../src/content/event');
-    const files = fs.readdirSync(dirPath);
-    const eventList = [];
-  
-    files.forEach((file) => {
-      const filePath = path.join(dirPath, file);
-      const eventData = processEventMarkdownFile(filePath);
-  
-      if (eventData) {
-          if (eventData.event_link === 'No event link given') {
-              console.error("No event link given") 
-            }else{
-                eventList.push(eventData);
-            }
+  const dirPath = path.join(__dirname, '../src/content/event');
+  const files = fs.readdirSync(dirPath);
+  const eventList = [];
+
+  files.forEach((file) => {
+    const filePath = path.join(dirPath, file);
+    const eventData = processEventMarkdownFile(filePath);
+
+    if (eventData) {
+      if (eventData.event_link === 'No event link given') {
+        console.error("No event link given")
+      } else {
+        eventList.push(eventData);
       }
-    });
-  
-    const sortedList = eventList.sort((a, b) => (a.id < b.id ? 1 : -1));
-    // TODO: make sure to only push {} object
-    let data = JSON.stringify(sortedList)
-    fs.writeFileSync("src/content/event/event.json", data)
-    return sortedList;
+    }
+  });
+
+  const sortedList = eventList.sort((a, b) => (a.id < b.id ? 1 : -1));
+  // TODO: make sure to only push {} object
+  let data = JSON.stringify(sortedList)
+  fs.writeFileSync("src/content/event/event.json", data)
+  return sortedList;
 };
-  
+
 const sortedArtistList = getSortedArtistList();
 const sortedEventList = getSortedEventsList();
 const sortedBannerList = getSortedBannerList();
